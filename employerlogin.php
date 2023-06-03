19e7d3209415ad701d8d321d791fea1e68aa74eb
wiserenn <akincilareren@gmail.com>
2023-06-03 16:13:06 +0300

    Changes to be committed: 	renamed:    employer.html -> employer.php 	modified:   employerlogin.php

--- employerlogin.php
+++ employerlogin.php
@@ -1,3 +1,30 @@
+<?php require_once 'connect_db.php'; 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM Users WHERE email = '$username' AND password = '$password'";
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC);
+    if ($row) {
+        $_SESSION["username"] = $row["username"];
+        header("Location: employer.php");
+        exit();
+    } else {
+        $error = "Invalid username or password";
+    }
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -12,17 +39,17 @@
             <div class="shape"></div>
             <div class="shape"></div>
         </div>
-    
+
     <form>
         <button type = "button" onclick="redirectToHome()">Go home page</button>
         <h3>Employer Login</h3>
-    
-        <label for="username">Username</label>
+
+        <label for="username">Company</label>
         <input type="text" placeholder="Email" id="username" required>
-    
+
         <label for="password">Password</label>
         <input type="password" placeholder="Password" id="password" required>
-    
+
         <button>Log In</button>
 
         <h4>Don't have an account? <a href="employerregister.html">Sign up</a></h6>
