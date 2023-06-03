ef27eaa2c099f9967f98f1633da9743af82de702
wiserenn <akincilareren@gmail.com>
2023-06-03 13:26:15 +0300

    modified:   connect_db.php

--- connect_db.php
+++ connect_db.php
@@ -9,7 +9,7 @@
     $conn = sqlsrv_connect( $servername, $connectionInfo);
 
 if( $conn ) {
-     echo "Connection established.<br />";
+     //echo "Connection established.<br />";
 }else{
 
      echo "Connection could not be established.<br />";
